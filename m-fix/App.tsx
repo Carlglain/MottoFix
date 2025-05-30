171eb3979f5c0131ceca95c898f4278d3da1a450
Carlstorm <carlglainnyuysemo@gmail.com>
2025-05-30 06:14:23 +0100

    Re-index m-fix folder

---
+++ m-fix/App.tsx
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to M-Fix App! this is not happening</Text>
+      <Text>Your safety our priority</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
